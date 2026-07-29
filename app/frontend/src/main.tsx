1fd7bae31170b0af9fee3c4142da25e0092649c2
kunifujiwara <kunihiko.9215.0920.research@gmail.com>
2026-07-30 02:24:58 +0900

    chore(ui): add @fontsource/inter and import 400/500/600 weights

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
